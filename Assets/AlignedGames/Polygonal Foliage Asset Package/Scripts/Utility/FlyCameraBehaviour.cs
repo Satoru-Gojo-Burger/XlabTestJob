6f7911499ae16f0ced85d94d20b8f3af7a3a2c70
Satoru-Gojo-Burger <grah228@yandex.ru>
2024-10-31 20:48:46 +0300

    Bug fix

--- Assets/AlignedGames/Polygonal Foliage Asset Package/Scripts/Utility/FlyCameraBehaviour.cs
+++ Assets/AlignedGames/Polygonal Foliage Asset Package/Scripts/Utility/FlyCameraBehaviour.cs
@@ -47,9 +47,9 @@ public class FlyCameraBehaviour : MonoBehaviour
         if (Input.GetKey(KeyCode.Q)) { transform.position += transform.up * climbSpeed * Time.deltaTime; }
         if (Input.GetKey(KeyCode.E)) { transform.position -= transform.up * climbSpeed * Time.deltaTime; }
 
-        if (Input.GetKeyDown(KeyCode.End))
-        {
-            Screen.lockCursor = (Screen.lockCursor == false) ? true : false;
-        }
+      //  if (Input.GetKeyDown(KeyCode.End))
+      //  {
+      //     Screen.lockCursor = (Screen.lockCursor == false) ? true : false;
+      //  }
     }
 }
